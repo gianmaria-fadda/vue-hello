2f55826badb08b81e0eae5c2e24ff64e2b0c8470
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-08-28 14:09:45 +0200

    aggiunto foto + pusharla nell'html (da correggere)

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
     createApp({
         data() {
             return {
-                message: 'Hello Vue!'
+                message: 'Hello Vue!',
+                imageSrc: './img/ronaldo-cristiano-2017-real-madrid-ballon-d-or-2016-0026751808hjpg-1698686328-120749.jpg'
             }
         }
     })
